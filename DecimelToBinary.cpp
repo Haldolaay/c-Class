644fef1bc0a8c198d60df7e2ec8dce04eb4a9b48
Hamzah Aldolaay <41927016+Haldolaay@users.noreply.github.com>
2020-08-24 21:46:13 -0700

    Create DecimelToBinary.cpp

---
+++ DecimelToBinary.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+ std::string addingZeroesToResult(std::string result){
+     if(result.size() < 16){
+       int i = 16 - result.size();
+     for ( ; i>0 ; i--){
+       result = "0"+ result;
+     }
+   }
+   return result;
+ }
+ std::string toBinary(unsigned short num){
+   std::string result = "";
+   while(num>0){
+     result = std::to_string(num % 2) + result;
+     num = num/2;
+   }
+
+   result = addingZeroesToResult(result);
+   return result;
+ };
+int main() {
+  int userInput;
+  std::cout<<"enter a number between 0 and 65535"<<std::endl;
+  std::cin>>userInput;
+  if(userInput > 65535 || userInput < 0){
+    std::cout<<"the input number is invalid";
+    return 0;
+  }
+  unsigned short value = (unsigned short)userInput;
+  std::cout<<toBinary(value)<<std::endl;
+  return 1;
+}
